f2d1047bcb33aff63f98c928b6eb787cb24ea8e7
taras <taras.romash@oa.edu.ua>
2026-03-24 14:02:06 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZNOWay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
