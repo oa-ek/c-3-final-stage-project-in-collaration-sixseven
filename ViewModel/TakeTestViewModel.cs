10299337586a6951148940e6d8ea3eefe60e0106
DenysIvasiuk <denys.ivasiuk@oa.edu.ua>
2026-03-25 04:10:19 +0200

    Add TakeTestViewModel, TestResultViewModel and updated views

---
+++ ViewModel/TakeTestViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ZNOWay.ViewModel
+{
+    public class TakeTestViewModel
+    {
+        public int TestId { get; set; }
+        public string TestName { get; set; } = string.Empty;
+        public string SubjectName { get; set; } = string.Empty;
+        public int TotalQuestions { get; set; }
+        public int? TimeLimit { get; set; }
+    }
+}
